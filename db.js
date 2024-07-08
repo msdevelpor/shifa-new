ca2739f585dc34be135f4534e5234a30c899d961
MSrasool2024 <>
2024-07-08 12:36:14 +0500

    change

--- db.js
+++ db.js
@@ -1,18 +1,19 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 module.exports = () => {
-	const connectionParams = {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useNewUrlParser: true, useCreateIndex: true
-	};
-	try {
-		mongoose.connect(process.env.DB);
-		
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  };
+  try {
+    mongoose.connect(process.env.DB);
+    //	change
 
-		console.log("Connected to database successfully");
-	} catch (error) {
-		console.log(error);
-		console.log("Could not connect database!");
-	}
+    console.log('Connected to database successfully');
+  } catch (error) {
+    console.log(error);
+    console.log('Could not connect database!');
+  }
 };
